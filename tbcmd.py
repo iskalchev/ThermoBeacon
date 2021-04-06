775025c1a75d3e1a7e404f44cccb816805125a5d
iskalchev <iskalchev@gmail.com>
2021-04-06 21:19:58 +0300

    commands...

--- tbcmd.py
+++ tbcmd.py
@@ -17,21 +17,12 @@ parser = ArgumentParser()
 subparsers = parser.add_subparsers(help='action', dest='command', required=True)
  
 sub = subparsers.add_parser('scan', help = "Scan for ThermoBeacon devices")
+sub.add_argument('-mac', type=mac_addr, required=False)
+sub.add_argument('-t', type=int, default = 20, metavar='<Scan duration, seconds>', required=False)
 sub = subparsers.add_parser('identify', help = "Identify a device")
 sub.add_argument('-mac', type=mac_addr, required=True)
-
-'''
-sub = subparsers.add_parser('add', help = "Add device")
+sub = subparsers.add_parser('query', help = "Query device for details")
 sub.add_argument('-mac', type=mac_addr, required=True)
-sub.add_argument('-name', default='no name')
-sub = subparsers.add_parser('remove', help = "Remove device")
-sub.add_argument('-mac', type=mac_addr, required=True)
-sub = subparsers.add_parser('config', help = 'Save configuration')
-sub.add_argument('-s', '--save', action='store_true', help='Save configuration to file')
-sub = subparsers.add_parser('discover', help = "Listen for device")
-sub.add_argument('-name', required=True, metavar='Name', help='Device Name')
-sub.add_argument('-t', type=int, choices=range(10,31), default=10, metavar='Timeout', help='Seconds to wait')
-'''
 
 
 args = parser.parse_args()
@@ -48,6 +39,9 @@ def main():
         except KeyboardInterrupt:
             print()
             return
+    elif cmd=='identify':
+        pass
+    print('Not yet implemented')
 
 async def scan():
     scanner = BleakScanner()
